896119b68017d8ef2995920a28b32d2b76cc7179
imWalsh <wshjay@live.cn>
2024-11-06 17:43:42 +0800

    Initial commit

--- lib/src/gallery_page.dart
+++ lib/src/gallery_page.dart
@@ -1,4 +1,4 @@
-import 'package:dismissible_page/dismissible_page.dart';
+// import 'package:dismissible_page/dismissible_page.dart';
 import 'package:flutter/material.dart';
 
 class GalleryPage extends StatelessWidget {
